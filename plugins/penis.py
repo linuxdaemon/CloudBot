38ca8a0569cd2a9606fb4860697196ecacaf9e71
linuxdaemon <linuxdaemon@snoonet.org>
2019-02-18 22:48:35 -0600

    Fix implicit string concat in sequence literals

--- plugins/penis.py
+++ plugins/penis.py
@@ -8,7 +8,7 @@ shaft = [
     '/////////////////////////', '|||||||||||||',
     '\u2248\u2248\u2248'
 ]
-head = ['D', 'Q', '>', '|\u2283' '\u22d1', '\u22d9', '\u22d7']
+head = ['D', 'Q', '>', '|\u2283', '\u22d1', '\u22d9', '\u22d7']
 emission = ['~ ~ ~ ~', '~ * ~ &', '', '*~* *~* %']
 bodypart = ['face', 'glasses', 'thigh', 'tummy', 'back', 'hiney', 'hair', 'boobs', 'tongue']
 
